aa8867f9215314a578c21ef13f2239080b590619
김성수 <kimseongsoo@gimseongsuui-MacBookAir.local>
2022-11-20 06:52:38 +0900

    가이드 레이아웃 버튼 변경완료 학습부분 각종 레이아웃 제작하기 외부api 끌어오기 => rest api 방식의 전달받을 자료들 자료형 정의하기(ex. int, char, 등등)

---
+++ app/src/main/java/com/example/listencarefree/TrainActivity.java
@@ -0,0 +1,17 @@
+package com.example.listencarefree;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TrainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_train);
+    }
+
+
+
+}
